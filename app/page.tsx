ab6711646c4b7ecbf1816840f59b09fbb341f7eb
Kairat Smakov <ksmakov@gmail.com>
2025-10-21 13:50:15 +0500

    test...

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,7 @@ import Monitoring from "./components/montoring";
 export default async function Page() {
   const baseUrl =
     process.env.NEXT_PUBLIC_SITE_URL ?? "https://storexmonitor.onrender.com";
-  const servers = await fetch(`${baseUrl}/api/ws`).then((res) => res.json());
-
-  return (
-    <div>
-      <Monitoring srvs={servers} />
-    </div>
-  );
+  //const servers = await fetch(`${baseUrl}/api/ws`).then((res) => res.json());
+  console.log(baseUrl);
+  return <div>{/* <Monitoring srvs={servers} /> */}</div>;
 }
